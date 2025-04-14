836b74e920f7fd2fea1781ee284828cef9d405d1
Juanfrancisco7 <juanbencomo25@gmail.com>
2025-04-14 19:59:35 +0200

    justify center nav

--- Js/footer.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.social-icon').forEach(icon => {
-    icon.addEventListener('click', function(e) {
-      e.preventDefault();
-      const link = this.getAttribute('href');
-      window.open(link, '_blank');
-    });
-  });
-  
