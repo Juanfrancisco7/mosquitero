fea88f0b0633bc7ef3eb3432c05ec6de8eb787e9
Juanfrancisco7 <juanbencomo25@gmail.com>
2025-04-15 10:08:05 +0200

    act js

--- Js/productos.js
+++ Js/productos.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const elementos = document.querySelectorAll('.fade-in');
-  
-    const animarScroll = new IntersectionObserver((entradas, observador) => {
-      entradas.forEach((entrada, index) => {
-        if (entrada.isIntersecting) {
-          entrada.target.style.transitionDelay = `${index * 0.2}s`;
-          entrada.target.classList.add('visible');
-          observador.unobserve(entrada.target);
-        }
-      });
-    }, {
-      threshold: 0.1
+  const elementos = document.querySelectorAll('.fade-in');
+
+  const animarScroll = new IntersectionObserver((entradas, observador) => {
+    entradas.forEach((entrada, index) => {
+      if (entrada.isIntersecting) {
+        entrada.target.style.transitionDelay = `${index * 0.2}s`;
+        entrada.target.classList.add('visible');
+        observador.unobserve(entrada.target);
+      }
     });
-  
-    elementos.forEach(el => animarScroll.observe(el));
+  }, {
+    threshold: 0.1
   });
-  
+
+  elementos.forEach(el => animarScroll.observe(el));
+});
